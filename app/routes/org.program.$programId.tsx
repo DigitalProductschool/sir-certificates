d821578af5f13d4218211dbde628e44777312384
Marcus Paeschke <marcus@mphasize.de>
2024-09-01 00:15:22 +0200

    Add list of certificates per batch and navigation between batches

--- app/routes/org.program.$programId.tsx
+++ app/routes/org.program.$programId.tsx
@@ -1,7 +1,16 @@
 import type { MetaFunction, LoaderFunction } from "@remix-run/node";
 import type { Program, Batch } from "@prisma/client";
+import { useEffect } from "react";
 import { json } from "@remix-run/node";
-import { NavLink, Link, Outlet, useLoaderData } from "@remix-run/react";
+import {
+  NavLink,
+  Link,
+  Outlet,
+  useLoaderData,
+  useNavigate,
+  useMatches,
+  useParams,
+} from "@remix-run/react";
 
 import {
   Select,
@@ -55,33 +64,76 @@ export const handle = {
 
 export default function ProgramPage() {
   const { program } = useLoaderData<typeof loader>();
+  const params = useParams();
+  const navigate = useNavigate();
+  const matches = useMatches();
 
-  const latestBatch = program.batches[program.batches.length - 1];
+  const latestBatch =
+    program.batches.length > 0
+      ? program.batches[program.batches.length - 1]
+      : undefined;
+
+  const handleBatchSelect = (value: string) => {
+    navigate(`/org/program/${program.id}/batch/${value}/certificates`);
+  };
+
+  useEffect(() => {
+    // IF at least one batch exists AND we're on program level THEN navigate to the latest batch
+    if (latestBatch && matches.length === 3) {
+      navigate(
+        `/org/program/${program.id}/batch/${latestBatch.id}/certificates`,
+      );
+    }
+  }, [program.id, matches, latestBatch, navigate]);
 
   return (
     <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-3">
       <div className="flex items-center gap-4">
-        <Select defaultValue={latestBatch.id.toString()}>
-          <SelectTrigger className="w-[280px]">
-            <SelectValue placeholder="Select a Batch" />
-          </SelectTrigger>
-          <SelectContent>
-            {program.batches.toReversed().map((batch: Batch) => (
-              <SelectItem key={batch.id} value={batch.id.toString()}>
-                {batch.name}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
+        {latestBatch && (
+          <>
+            <Select
+              key={params.batchId}
+              defaultValue={params.batchId}
+              onValueChange={handleBatchSelect}
+            >
+              <SelectTrigger className="w-[280px]">
+                <SelectValue placeholder="Select a Batch" />
+              </SelectTrigger>
+              <SelectContent>
+                {program.batches.toReversed().map((batch: Batch) => (
+                  <SelectItem key={batch.id} value={batch.id.toString()}>
+                    {batch.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
 
-        <NavLink
-          to={`batch/${latestBatch.id}/import`}
-          className={({ isActive }) =>
-            isActive ? "bg-background p-2 rounded-md" : "p-2 rounded-md"
-          }
-        >
-          Import Participants
-        </NavLink>
+            {matches.length > 3 && (
+              <div className="inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground">
+                <NavLink
+                  to={`batch/${params.batchId}/certificates`}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-background text-foreground shadow-sm rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background"
+                      : "rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background"
+                  }
+                >
+                  Certificates
+                </NavLink>
+                <NavLink
+                  to={`batch/${params.batchId}/import`}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-background text-foreground shadow-sm rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background"
+                      : "rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background"
+                  }
+                >
+                  Import Participants
+                </NavLink>
+              </div>
+            )}
+          </>
+        )}
 
         {program.batches.length === 0 && (
           <div>No batches added yet. Create your first batch.</div>
