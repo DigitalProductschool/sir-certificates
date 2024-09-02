e987a78dc038aa364600c365c5dc762b5e6b1d77
Marcus Paeschke <marcus@mphasize.de>
2024-09-02 10:23:28 +0200

    Improve error handling

--- app/lib/prisma.server.ts
+++ app/lib/prisma.server.ts
@@ -1,8 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+/* eslint-disable no-var */
 
+import { PrismaClient, Prisma } from "@prisma/client";
+const PrismaClientKnownRequestError = Prisma.PrismaClientKnownRequestError;
+const PrismaClientUnknownRequestError = Prisma.PrismaClientUnknownRequestError;
+const PrismaClientInitializationError = Prisma.PrismaClientInitializationError;
+const PrismaClientValidationError = Prisma.PrismaClientValidationError;
+const PrismaClientRustPanicError = Prisma.PrismaClientRustPanicError;
+
+// Create and export global PrismaClient
 let prisma: PrismaClient;
 declare global {
-	var __db: PrismaClient | undefined;
+  var __db: PrismaClient | undefined;
 }
 
 if (process.env.NODE_ENV === "production") {
@@ -17,4 +25,38 @@ if (process.env.NODE_ENV === "production") {
 	prisma = global.__db;
 }
 
-export { prisma };
+const throwErrorResponse = (
+	error:
+		| Error
+		| typeof PrismaClientKnownRequestError
+		| typeof PrismaClientUnknownRequestError
+		| typeof PrismaClientInitializationError
+		| typeof PrismaClientInitializationError
+		| typeof PrismaClientValidationError
+		| typeof PrismaClientRustPanicError,
+	info: string,
+) => {
+	let errorMessage = info;
+	if (error instanceof PrismaClientKnownRequestError) {
+		errorMessage = `${info}. ${error.meta ? `Check ${JSON.stringify(error.meta?.target)}.` : ""} (Error ${error.code})`;
+		throw new Response(errorMessage, {
+			status: 400,
+			statusText: errorMessage,
+		});
+	}
+
+	throw new Response(errorMessage, {
+		status: 500,
+		statusText: errorMessage,
+	});
+};
+
+export {
+	prisma,
+	throwErrorResponse,
+	PrismaClientKnownRequestError,
+	PrismaClientUnknownRequestError,
+	PrismaClientInitializationError,
+	PrismaClientValidationError,
+	PrismaClientRustPanicError,
+};
--- app/routes/api.import.ts
+++ app/routes/api.import.ts
@@ -2,7 +2,7 @@ import type { ActionFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
 import { requireUserId } from "~/lib/auth.server";
-import { prisma } from "~/lib/prisma.server";
+import { prisma, throwErrorResponse } from "~/lib/prisma.server";
 
 export const action: ActionFunction = async ({ request }) => {
 	// @todo require admin user
@@ -14,29 +14,31 @@ export const action: ActionFunction = async ({ request }) => {
 	// @todo validate inputs? (or rely on Prisma internal validation?)
 
 	// If this email exists already for this batch, update instead of create
-	const certificate = await prisma.certificate.upsert({
-		where: {
-			certId: {
+	const certificate = await prisma.certificate
+		.upsert({
+			where: {
+				certId: {
+					email: inputs.email,
+					batchId: 0, // Number(inputs.batchId),
+				},
+			},
+			update: {
+				firstName: inputs.firstName,
+				lastName: inputs.lastName,
+				// @todo team, track
+			},
+			create: {
+				firstName: inputs.firstName,
+				lastName: inputs.lastName,
 				email: inputs.email,
-				batchId: Number(inputs.batchId),
+				batch: {
+					connect: { id: Number(inputs.batchId) },
+				},
 			},
-		},
-		update: {
-			firstName: inputs.firstName,
-			lastName: inputs.lastName,
-			// @todo team, track
-		},
-		create: {
-			firstName: inputs.firstName,
-			lastName: inputs.lastName,
-			email: inputs.email,
-			batch: {
-				connect: { id: Number(inputs.batchId) },
-			},
-		},
-	});
-
-	// @todo error handling for Prisma create
+		})
+		.catch((error) => {
+			throwErrorResponse(error, "Could not import certificate");
+		});
 
 	return json({ certificate });
 };
