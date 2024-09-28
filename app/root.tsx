a343806f4da02aa67587a3e02185d2bef15741e4
Marcus Paeschke <marcus@mphasize.de>
2024-09-28 17:31:11 +0200

    Remove stack trace from public error

--- app/root.tsx
+++ app/root.tsx
@@ -62,8 +62,6 @@ export function ErrorBoundary() {
       <div>
         <h1>Error</h1>
         <p>{error.message}</p>
-        <p>The stack trace is:</p>
-        <pre>{error.stack}</pre>
       </div>
     );
   } else {
