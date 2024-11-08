5ebe4346ea69940b7f8bcfe2749733d696b841ec
Marcus Paeschke <marcus@mphasize.de>
2024-11-08 18:57:43 +0100

    Information about the program (about) and the certificate (achievement) as well as a link to the program website can be configured

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,18 @@
 import type { MetaFunction, LoaderFunction } from "@remix-run/node";
+import type { Program } from "@prisma/client";
 import type { CertificatesWithBatchAndProgram } from "./view";
 import { json } from "@remix-run/node";
-import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import Markdown from "markdown-to-jsx";
 import { SidebarParticipant } from "~/components/sidebar-participant";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
 import {
   SidebarInset,
   SidebarProvider,
@@ -30,6 +40,8 @@ export const loader: LoaderFunction = async ({ request }) => {
     });
   }
 
+  // @todo parallelize DB requests instead of awaiting each one
+
   let org = await prisma.organisation.findUnique({
     where: {
       id: 1,
@@ -39,6 +51,12 @@ export const loader: LoaderFunction = async ({ request }) => {
   if (!org) {
     org = { id: 1, name: "Unknown Organisation" };
   }
+
+  const programs = await prisma.program.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
 
   const certificates = await prisma.certificate.findMany({
     where: {
@@ -53,15 +71,13 @@ export const loader: LoaderFunction = async ({ request }) => {
     },
   });
 
-  return json({ user, org, certificates });
+  return json({ user, org, programs, certificates });
 };
 
 export default function Index() {
-  const { user, org, certificates } = useLoaderData<typeof loader>();
+  const { user, org, programs, certificates } = useLoaderData<typeof loader>();
 
   // @todo fix unintended change of open/close state of the sidebar when navigating between _index and /view
-
-  // @todo version with no certificates yet --> show programs and application options?
 
   return (
     <div className="flex min-h-screen w-full">
@@ -74,27 +90,74 @@ export default function Index() {
           </header>
 
           <h1 className="text-5xl font-bold px-16">
-            Congratulations {user?.firstName}!
+            {certificates.length === 0 ? "Hi" : "Congratulations"}{" "}
+            {user?.firstName}!
           </h1>
 
-          <p className="px-16">
-            Here are all your certificates from {org.name}
+          <p className="px-16 max-w-[70ch] text-balance">
+            {certificates.length === 0
+              ? `It looks like you have not received any certificates from ${org.name} yet. If your certificate is missing, please talk to your program coordinator. If you have not joined any program yet, have a look at the programs offering certification below.`
+              : `Here are all your certificates from ${org.name}`}
           </p>
 
-          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 p-16 gap-16">
+          <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 px-16 py-12 gap-16">
             {certificates.map((cert: CertificatesWithBatchAndProgram) => (
-              <Link to={`/view/${cert.uuid}`} key={cert.id}>
+              <Link
+                to={`/view/${cert.uuid}`}
+                key={cert.id}
+                className="flex flex-col gap-2"
+              >
+                <span className="text-center">
+                  <b>{cert.batch.program.name}</b> &mdash; {cert.batch.name}
+                </span>
                 <img
                   className="w-full mb-4 drop-shadow-xl hover:drop-shadow-lg hover:opacity-85"
                   src={`/cert/${cert.uuid}/preview.png?t=${cert.updatedAt}`}
                   alt={`Preview of your ${cert.batch.program.name} certificate`}
                 />
-                <b>{cert.batch.program.name}</b> &mdash; {cert.batch.name}
+                <Button variant="outline">Open certificate</Button>
               </Link>
             ))}
           </div>
 
-          <Outlet />
+          {programs.length > 1 && (
+            <>
+              <div>
+                <h3 className="text-lg font-bold px-16">
+                  More offers from {org.name}
+                </h3>
+                <p className="text-sm   px-16">
+                  If you are interested in receiving more certifications, have a
+                  look at our other programs.
+                </p>
+              </div>
+              <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 p-16 pt-2 gap-8">
+                {programs.map((program: Program) => (
+                  <Card key={program.id}>
+                    <CardHeader>
+                      <CardTitle className="text-xl">{program.name}</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <Markdown>{program.about ?? ""}</Markdown>
+                    </CardContent>
+                    <CardFooter>
+                      {/* @todo insert <wbr> tags into long urls for better breakpoints */}
+                      {program.website && (
+                        <a
+                          href={program.website}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="underline underline-offset-2 break-all"
+                        >
+                          {program.website}
+                        </a>
+                      )}
+                    </CardFooter>
+                  </Card>
+                ))}
+              </div>
+            </>
+          )}
         </SidebarInset>
       </SidebarProvider>
     </div>
