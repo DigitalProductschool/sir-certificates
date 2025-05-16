e221b01c6adb80566cc2686cf98e2d8a608056d2
Marcus Paeschke <marcus@mphasize.de>
2025-05-16 10:35:43 +0200

    Add option to remove program logo

--- app/routes/org.program.$programId.settings.logo-delete.tsx
+++ app/routes/org.program.$programId.settings.logo-delete.tsx
@@ -2,10 +2,7 @@ import type { ActionFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 
 import { requireAdmin } from "~/lib/auth.server";
-import {
-  deleteSocialBackground,
-  deleteSocialComposites,
-} from "~/lib/social.server";
+import { deleteProgramLogo } from "~/lib/program.server";
 import { prisma } from "~/lib/prisma.server";
 import { requireAccessToProgram } from "~/lib/program.server";
 
@@ -13,30 +10,25 @@ export const action: ActionFunction = async ({ request, params }) => {
   const adminId = await requireAdmin(request);
   requireAccessToProgram(adminId, Number(params.programId));
 
-  // Clean up existing background image
-  const existingSocial = await prisma.socialPreview.findFirst({
+  // Clean up existing logo image
+  const existingLogo = await prisma.programLogo.findUnique({
     where: {
       programId: Number(params.programId),
     },
   });
-  if (existingSocial) {
+  if (existingLogo) {
     try {
-      await deleteSocialBackground(existingSocial);
-    } catch (error) {
-      // If the file was not on disk, we ignore that and proceed with deleting the record
-    }
-    try {
-      await deleteSocialComposites(existingSocial.id);
+      await deleteProgramLogo(existingLogo);
     } catch (error) {
       // If the file was not on disk, we ignore that and proceed with deleting the record
     }
 
-    await prisma.socialPreview.delete({
+    await prisma.programLogo.delete({
       where: {
-        id: existingSocial.id,
+        id: existingLogo.id,
       },
     });
   }
 
-  return redirect(`/org/program/${params.programId}/social`);
+  return redirect(`/org/program/${params.programId}/settings`);
 };
