f87c22b5fbb45af8b43b339b5816db9748790d0f
Marcus Paeschke <marcus@mphasize.de>
2025-05-30 16:34:55 +0200

    Update Remix.run dependencies, activate Future flags, install new file-based routes.ts depedencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
 export default defineConfig({
   ssr: {
     noExternal: ["react-easy-crop", "tslib"],
@@ -9,9 +15,13 @@ export default defineConfig({
   plugins: [
     remix({
       future: {
+        unstable_optimizeDeps: true,
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+        v3_routeConfig: true,
       },
     }),
     tsconfigPaths(),
