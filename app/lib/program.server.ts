b7ae16227fc67dd6faa1a21dd905f38205c948ec
Marcus Paeschke <marcus@mphasize.de>
2025-06-27 11:49:01 +0200

    Refactor unstable upload handler for program settings logo upload, more to follow

--- app/lib/program.server.ts
+++ app/lib/program.server.ts
@@ -1,126 +1,132 @@
 import type { Program, ProgramLogo } from "@prisma/client";
-
-import { prisma } from "./prisma.server";
 import { resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-import { writeFile, unlink } from "node:fs/promises";
+import { unlink } from "node:fs/promises";
+import {
+  openFile as lazyOpenFile,
+  writeFile as lazyWriteFile,
+} from "@mjackson/lazy-file/fs";
 import { ensureFolderExists, readFileIfExists } from "./fs.server";
+import { prisma } from "./prisma.server";
+import { FileUpload } from "@mjackson/form-data-parser";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
-const logoDir = resolve(__dirname, "../../storage/logos");
+export const logoDir = resolve(__dirname, "../../storage/logos");
 
 export async function getProgramsByAdmin(adminId: number, include = {}) {
-	const admin = await prisma.user.findUnique({
-		where: {
-			id: adminId,
-		},
-	});
+  const admin = await prisma.user.findUnique({
+    where: {
+      id: adminId,
+    },
+  });
 
-	const filterPrograms = admin?.isSuperAdmin
-		? undefined
-		: {
-				admins: {
-					some: {
-						id: adminId,
-					},
-				},
-			};
+  const filterPrograms = admin?.isSuperAdmin
+    ? undefined
+    : {
+        admins: {
+          some: {
+            id: adminId,
+          },
+        },
+      };
 
-	return await prisma.program.findMany({
-		where: filterPrograms,
-		include,
-		orderBy: {
-			name: "asc",
-		},
-	});
+  return await prisma.program.findMany({
+    where: filterPrograms,
+    include,
+    orderBy: {
+      name: "asc",
+    },
+  });
 }
 
 export async function requireAccessToProgram(
-	adminId: number,
-	programId: number,
+  adminId: number,
+  programId: number,
 ) {
-	const admin = await prisma.user.findUnique({
-		where: {
-			id: adminId,
-		},
-		select: {
-			id: true,
-			email: true,
-			firstName: true,
-			lastName: true,
-			isAdmin: true,
-			isSuperAdmin: true,
-			adminOfPrograms: true,
-		},
-	});
+  const admin = await prisma.user.findUnique({
+    where: {
+      id: adminId,
+    },
+    select: {
+      id: true,
+      email: true,
+      firstName: true,
+      lastName: true,
+      isAdmin: true,
+      isSuperAdmin: true,
+      adminOfPrograms: true,
+    },
+  });
 
-	if (!admin || !(admin.isAdmin || admin.isSuperAdmin)) {
-		throw new Response(null, {
-			status: 403,
-			statusText: "You need to be a program manager to access this page.",
-		});
-	}
+  if (!admin || !(admin.isAdmin || admin.isSuperAdmin)) {
+    throw new Response(null, {
+      status: 403,
+      statusText: "You need to be a program manager to access this page.",
+    });
+  }
 
-	if (admin.isSuperAdmin) {
-		return admin;
-	}
+  if (admin.isSuperAdmin) {
+    return admin;
+  }
 
-	if (
-		admin.isAdmin &&
-		admin.adminOfPrograms.some(
-			(program: Program) => program.id === programId,
-		)
-	) {
-		return admin;
-	}
+  if (
+    admin.isAdmin &&
+    admin.adminOfPrograms.some((program: Program) => program.id === programId)
+  ) {
+    return admin;
+  }
 
-	throw new Response(null, {
-		status: 403,
-		statusText: "You do not have access to this program.",
-	});
+  throw new Response(null, {
+    status: 403,
+    statusText: "You do not have access to this program.",
+  });
 }
 
-export async function saveProgramLogo(logo: ProgramLogo, image: File) {
-	const folderCreated = await ensureFolderExists(logoDir);
-	if (!folderCreated) {
-		throw new Error("Could not create social storage folder");
-	}
+export async function saveProgramLogoUpload(
+  logo: ProgramLogo,
+  image: FileUpload,
+) {
+  const folderCreated = await ensureFolderExists(logoDir);
+  if (!folderCreated) {
+    throw new Error("Could not create social storage folder");
+  }
 
-	let extension: "svg" | "unkown";
-	switch (image.type) {
-		case "image/svg+xml":
-			extension = "svg";
-			break;
-		default:
-			extension = "unkown";
-	}
+  let extension: "svg" | "unkown";
+  switch (image.type) {
+    case "image/svg+xml":
+      extension = "svg";
+      break;
+    default:
+      extension = "unkown";
+  }
 
-	const buffer = Buffer.from(await image.arrayBuffer());
-	return await writeFile(`${logoDir}/${logo.id}.logo.${extension}`, buffer);
+  const filepath = `${logoDir}/${logo.id}.logo.${extension}`;
+  await lazyWriteFile(filepath, image);
+  return lazyOpenFile(filepath);
 }
 
 export async function readProgramLogo(logo: ProgramLogo) {
-	let extension: "svg" | "unkown";
-	switch (logo.contentType) {
-		case "image/svg+xml":
-			extension = "svg";
-			break;
-		default:
-			extension = "unkown";
-	}
+  let extension: "svg" | "unkown";
+  switch (logo.contentType) {
+    case "image/svg+xml":
+      extension = "svg";
+      break;
+    default:
+      extension = "unkown";
+  }
 
-	return await readFileIfExists(`${logoDir}/${logo.id}.logo.${extension}`);
+  return await readFileIfExists(`${logoDir}/${logo.id}.logo.${extension}`);
 }
 
 export async function deleteProgramLogo(logo: ProgramLogo) {
-	let extension: "svg" | "unkown";
-	switch (logo.contentType) {
-		case "image/svg+xml":
-			extension = "svg";
-			break;
-		default:
-			extension = "unkown";
-	}
+  let extension: "svg" | "unkown";
+  switch (logo.contentType) {
+    case "image/svg+xml":
+      extension = "svg";
+      break;
+    default:
+      extension = "unkown";
+  }
 
-	return await unlink(`${logoDir}/${logo.id}.logo.${extension}`);
+  return await unlink(`${logoDir}/${logo.id}.logo.${extension}`);
 }
