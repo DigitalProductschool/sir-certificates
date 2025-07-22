71e8599c0090765fe3a8bfdc5573988da82a5e26
Marcus Paeschke <marcus@mphasize.de>
2025-07-22 17:34:17 +0200

    Org settings for senderName and senderEmail, refactored UI (no more Dialog)

--- app/routes/org.settings.tsx
+++ app/routes/org.settings.tsx
@@ -1,26 +1,23 @@
 import type { Route } from "./+types/org.settings";
-import { useEffect, useState } from "react";
-import { Form, redirect, useNavigate } from "react-router";
+import { FormUpdate } from "~/components/form-update";
 
-import { Button } from "~/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogFooter,
-  DialogTitle,
-} from "~/components/ui/dialog";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 
 import { requireSuperAdmin } from "~/lib/auth.server";
-import { getOrg } from "~/lib/organisation.server";
-import { prisma } from "~/lib/prisma.server";
+import { getOrg, saveOrg } from "~/lib/organisation.server";
 
 export function meta() {
   return [{ title: "Edit Organisation" }];
 }
+
+const allowedUpdateFields = [
+  "name",
+  "imprintUrl",
+  "privacyUrl",
+  "senderEmail",
+  "senderName",
+];
 
 export async function action({ request }: Route.ActionArgs) {
   await requireSuperAdmin(request);
@@ -28,19 +25,18 @@ export async function action({ request }: Route.ActionArgs) {
   const formData = await request.formData();
   const inputs = Object.fromEntries(formData) as { [k: string]: string };
 
-  await prisma.organisation.update({
-    where: {
-      id: 1,
-    },
-    data: {
-      name: inputs.name,
-      imprintUrl: inputs.imprintUrl,
-      privacyUrl: inputs.privacyUrl,
-    },
+  const update: { [key: string]: string } = {};
+
+  allowedUpdateFields.forEach((field) => {
+    if (inputs[field]) {
+      update[field] = inputs[field].trim();
+    }
   });
 
-  // @todo since settings is reachable globally, redirecting back to program is not always correct
-  return redirect(`/org/program`);
+  // @todo validate senderEmail as email
+
+  const org = await saveOrg(update);
+  return { org };
 }
 
 export async function loader({ request }: Route.LoaderArgs) {
@@ -49,63 +45,71 @@ export async function loader({ request }: Route.LoaderArgs) {
   return { org };
 }
 
-export default function EditOrgDialog({ loaderData }: Route.ComponentProps) {
+export default function OrgSettings({ loaderData }: Route.ComponentProps) {
   const { org } = loaderData;
-  const navigate = useNavigate();
-  const [open, setOpen] = useState(true);
-
-  useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
-        e.preventDefault();
-        setOpen(false);
-      }
-    };
-
-    document.addEventListener("keydown", down);
-    return () => document.removeEventListener("keydown", down);
-  }, [navigate]);
 
   return (
-    <Dialog
-      open={open}
-      onOpenChange={(open) => {
-        if (!open) navigate(-1);
-      }}
-    >
-      <DialogContent className="sm:max-w-[425px]">
-        <Form method="POST">
-          <DialogHeader>
-            <DialogTitle>Organisation settings</DialogTitle>
-            <DialogDescription>
-              Change the information as needed.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <Label htmlFor="name">Name of your organisation</Label>
-            <Input id="name" name="name" defaultValue={org.name} />
+    <div className="grid gap-8 py-4 max-w-[625px]">
+      <section className="flex flex-col gap-2">
+        <Label htmlFor="name">Name of your organisation</Label>
+        <FormUpdate key={`name-${org?.updatedAt}`}>
+          <Input id="name" name="name" defaultValue={org.name} />
+        </FormUpdate>
+      </section>
 
-            <Label htmlFor="imprintUrl">Imprint URL</Label>
-            <Input
-              id="imprintUrl"
-              name="imprintUrl"
-              defaultValue={org.imprintUrl ?? ""}
-              placeholder="https://"
-            />
+      <section className="flex flex-col gap-2">
+        <h2>Legal</h2>
+        <Label htmlFor="imprintUrl" className="mt-2">
+          Imprint URL
+        </Label>
+        <FormUpdate key={`imprint-${org?.updatedAt}`}>
+          <Input
+            id="imprintUrl"
+            name="imprintUrl"
+            defaultValue={org.imprintUrl ?? ""}
+            placeholder="https://"
+          />
+        </FormUpdate>
 
-            <Label htmlFor="privacyUrl">Privacy Policy URL</Label>
-            <Input
-              id="privacyUrl"
-              name="privacyUrl"
-              defaultValue={org.privacyUrl ?? ""}
-              placeholder="https://"
-            />
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </Form>
-      </DialogContent>
-    </Dialog>
+        <Label htmlFor="privacyUrl" className="mt-2">
+          Privacy Policy URL
+        </Label>
+        <FormUpdate key={`privacy-${org?.updatedAt}`}>
+          <Input
+            id="privacyUrl"
+            name="privacyUrl"
+            defaultValue={org.privacyUrl ?? ""}
+            placeholder="https://"
+          />
+        </FormUpdate>
+      </section>
+
+      <section className="flex flex-col gap-2">
+        <h2>Notifications</h2>
+
+        <Label htmlFor="senderEmail" className="mt-2">
+          Sender Email
+        </Label>
+        <FormUpdate key={`email-${org?.updatedAt}`}>
+          <Input
+            id="senderEmail"
+            name="senderEmail"
+            defaultValue={org.senderEmail ?? ""}
+            placeholder="@"
+          />
+        </FormUpdate>
+
+        <Label htmlFor="senderName" className="mt-2">
+          Sender Name
+        </Label>
+        <FormUpdate key={`sender-${org?.updatedAt}`}>
+          <Input
+            id="senderName"
+            name="senderName"
+            defaultValue={org.senderName ?? ""}
+          />
+        </FormUpdate>
+      </section>
+    </div>
   );
 }
