da21c2831149aa1c360d29d22bcf8159282b0628
Marcus Paeschke <marcus@mphasize.de>
2026-02-06 16:30:16 +0100

    Redirect admins to internal program overview on login

--- app/routes/auth.google.callback.ts
+++ app/routes/auth.google.callback.ts
@@ -4,5 +4,8 @@ import { authenticator, createUserSessionAndRedirect } from "~/lib/auth.server";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const user = await authenticator.authenticate("google", request);
-  return createUserSessionAndRedirect(user, "/");
+  return createUserSessionAndRedirect(
+    user,
+    user.isAdmin || user.isSuperAdmin ? "/org/program" : "/",
+  );
 }
