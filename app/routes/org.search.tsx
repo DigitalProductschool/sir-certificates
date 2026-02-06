756993e5790edea553db0c520e88a77d448e735a
Marcus Paeschke <marcus@mphasize.de>
2026-02-06 16:53:38 +0100

    Improve search result presentation (with a table for now)

--- app/routes/org.search.tsx
+++ app/routes/org.search.tsx
@@ -5,6 +5,21 @@ import { Link, useRouteLoaderData } from "react-router";
 import { requireAdmin } from "~/lib/auth.server";
 import { getProgramsByAdmin } from "~/lib/program.server";
 import { prisma } from "~/lib/prisma.server";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+import { MailCheck } from "lucide-react";
+import { Button } from "~/components/ui/button";
 
 export function meta() {
   return [{ title: "Search Organisation" }];
@@ -52,6 +67,8 @@ export async function loader({ request }: Route.LoaderArgs) {
       firstName: true,
       lastName: true,
       teamName: true,
+      email: true,
+      notifiedAt: true,
       batch: {
         select: {
           id: true,
@@ -84,24 +101,73 @@ export default function OrgSearchResults({ loaderData }: Route.ComponentProps) {
   const certificates = loaderData.results.certificates;
 
   return (
-    <div className="grid gap-8 py-4 max-w-[625px]">
+    <div className="flex flex-col gap-4">
       <p>
-        Search results for ›{term}‹ in {org?.name}.
+        Search results for ›{term}‹ in {org?.name}.<br />
+        <span className="text-sm text-muted-foreground">
+          Found {certificates.length} certificate
+          {certificates.length !== 1 ? "s" : ""}.
+        </span>
       </p>
-      Found {certificates.length} certificate(s).
-      <ul>
-        {certificates.map((cert) => (
-          <li key={cert.uuid}>
-            <Link
-              to={`/org/program/${cert.batch.program.id}/batch/${cert.batch.id}/certificates/${cert.id}/preview`}
-              className="hover:underline"
-            >
-              {cert.firstName} {cert.lastName} – Team: {cert.teamName} – Batch:{" "}
-              {cert.batch.name} – Program: {cert.batch.program.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead className="font-medium">Email</TableHead>
+            <TableHead>Team</TableHead>
+            <TableHead>Batch</TableHead>
+            <TableHead>Program</TableHead>
+            <TableHead colSpan={2}>Actions</TableHead>
+            <TableHead></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {certificates.map((cert) => {
+            return (
+              <TableRow key={cert.uuid}>
+                <TableCell>
+                  {cert.firstName} {cert.lastName}
+                </TableCell>
+                <TableCell className="font-medium">{cert.email}</TableCell>
+                <TableCell>{cert.teamName}</TableCell>
+                <TableCell>
+                  <Link
+                    to={`/org/program/${cert.batch.program.id}/batch/${cert.batch.id}/certificates`}
+                    className="hover:underline"
+                  >
+                    {cert.batch.name}
+                  </Link>
+                </TableCell>
+                <TableCell>{cert.batch.program.name}</TableCell>
+                <TableCell>
+                  <Button variant="outline" asChild>
+                    <Link
+                      to={`/org/program/${cert.batch.program.id}/batch/${cert.batch.id}/certificates/${cert.id}/preview`}
+                      className="hover:underline"
+                    >
+                      Open
+                    </Link>
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  {cert.notifiedAt ? (
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <MailCheck />
+                      </TooltipTrigger>
+                      <TooltipContent side="top">
+                        {cert.notifiedAt.toLocaleString("en-UK")}
+                      </TooltipContent>
+                    </Tooltip>
+                  ) : (
+                    <>&emsp;</>
+                  )}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
     </div>
   );
 }
